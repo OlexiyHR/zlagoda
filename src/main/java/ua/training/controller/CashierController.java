d4cb2e94fdb5bae6bc1d00d5af183605a7673f15
OlexiyHR <groleks30@gmail.com>
2024-06-23 09:09:40 +0300

    zlagoda 5

---
+++ src/main/java/ua/training/controller/CashierController.java
@@ -0,0 +1,60 @@
+package ua.training.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.apache.log4j.Logger;
+import ua.training.constants.Attribute;
+import ua.training.constants.ServletPath;
+import ua.training.controller.command.Command;
+import ua.training.controller.command.i18n.AppLocale;
+import ua.training.controller.filter.FilterAccess;
+import ua.training.controller.utils.CommandKeyGenerator;
+import ua.training.controller.utils.HttpWrapper;
+import java.io.IOException;
+import java.text.ParseException;
+
+@WebServlet("/cashier")
+public class CashierController extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(LoginController.class);
+    private static final long serialVersionUID = 1L;
+
+    public CashierController() {
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        getServletContext().setAttribute(Attribute.LOCALES, AppLocale.getAppLocales());
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (FilterAccess.getInstance().filterAccess(request, "Касир")) {
+            request.getRequestDispatcher("/WEB-INF/views/cashier.jsp").forward(request, response);
+        } else if (FilterAccess.getInstance().filterAccess(request, "Менеджер")) {
+            response.sendRedirect("/cashier");
+        } else response.sendRedirect("/login");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpWrapper httpWrapper = new HttpWrapper(request, response);
+        String commandKey = CommandKeyGenerator.generateCommandKeyFromRequest(request);
+        Command command = CommandFactory.getCommand(commandKey);
+        try {
+            String commandResultedResource = command.execute(request, response);
+            response.sendRedirect("/login");
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
